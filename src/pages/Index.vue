3db0c03874f71a7a701ad97bbe294501319ce4e5
Stathis96 <ioannidis.stathis@yahoo.gr>
2022-01-22 20:14:04 +0200

    Create Index.vue

---
+++ src/pages/Index.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page class="row items-center justify-evenly" style="background-color:SlateBlue;">
+    <HomePage/>
+  </q-page>
+</template>
+
+<script lang="ts">
+import HomePage from 'src/pages/HomePage.vue'
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'PageIndex',
+  components: { HomePage }
+})
+</script>
