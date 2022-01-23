48ba337dd4e868edcf3791ac6b6cb71edfd67bb6
Stathis96 <ioannidis.stathis@yahoo.gr>
2022-01-23 19:23:45 +0200

    New page
    Seperating cand to new and old with this page

---
+++ src/pages/Oldnew.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="q-pa-md">
+      <h5 class="fonted inset-shadow-down text-italic text-center text-bold">
+         Interns
+      </h5>
+
+        <div class="q-gutter-y-md " style="max-width: auto">
+          <q-card>
+            <q-tabs
+              v-model="tab"
+              dense
+              class="text-grey"
+              active-color="primary"
+              indicator-color="primary"
+              align="justify"
+              narrow-indicator
+            >
+              <q-tab name="active" class="tableletters" label="Active Interns" />
+              <q-tab name="old" class="tableletters" label="Old Interns" />
+            </q-tabs>
+            <q-separator />
+
+          </q-card>
+          </div>
+  </div>
+</template>
+
+<script lang='ts'>
+import { defineComponent, ref } from 'vue'
+import { useFetchInterns, useFetchOtherInterns } from 'src/hooks/useFetchInterns'
+
+export default defineComponent({
+  name: 'Interviews',
+  components: {
+  },
+  setup () {
+    const tab = ref('active')
+
+    return {
+      tab: ref('active')
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
