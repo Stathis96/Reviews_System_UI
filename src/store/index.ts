42a4c5ab4872f037282e34af22c6db5d2789ad2b
Stathis96 <ioannidis.stathis@yahoo.gr>
2022-01-26 13:15:11 +0200

    Creating store with pinia

---
+++ src/store/index.ts
@@ -0,0 +1,19 @@
+import { createPinia } from 'pinia'
+import { store } from 'quasar/wrappers'
+
+// import example from './module-example'
+
+/*
+ * If not building with SSR mode, you can
+ * directly export the Store instantiation;
+ *
+ * The function below can be async too; either use
+ * async/await or return a Promise which resolves
+ * with the Store instance.
+ */
+
+export default store(function (/* { ssrContext } */) {
+  const Store = createPinia()
+
+  return Store
+})
