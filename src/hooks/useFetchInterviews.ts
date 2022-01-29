8e3fb5f25846a2d05c6cb21cf2b474fc98519902
Stathis96 <ioannidis.stathis@yahoo.gr>
2022-01-29 20:10:47 +0200

    fully completed Crud interviews

--- src/hooks/useFetchInterviews.ts
+++ src/hooks/useFetchInterviews.ts
@@ -81,3 +81,130 @@ export function useFetchOldInterviews () {
     fetchInterviews2
   }
 }
+
+export function useFetchPendingInterviews (status: Ref<string>) {
+  const pendingResult = ref<Interview[]>([])
+  const loading = ref(false)
+
+  const fetchInterviews = async () => {
+    try {
+      loading.value = true
+      const response = await api({
+        url: '',
+        method: 'post',
+        data: {
+          query: print(getInterviewsByStatus),
+          variables: {
+            getInterviewsByStatus: unref(status)
+          }
+        }
+      }) as unknown as GraphQLResponse <{ pendingInterviews: Interview[]}>
+
+      if (response.data.data) {
+        // console.log(response.data.data.pendingInterviews)
+        pendingResult.value = response.data.data.pendingInterviews
+      }
+    } catch (e) {
+      console.log(e)
+    } finally {
+      loading.value = false
+    }
+  }
+  onMounted(async () => {
+    await fetchInterviews().catch(e => console.log(e))
+  })
+
+  watch(status, async () => {
+    // console.log('watcher')
+    // console.log('val:', val)
+    // console.log('prev', prev)
+    await fetchInterviews()
+  })
+
+  return {
+    pendingResult,
+    loading,
+    fetchInterviews
+  }
+}
+
+export function useFetchStatsForInterviews () {
+  const interviewsResult = ref<StatusStats[]>([])
+  const loading = ref(false)
+
+  const fetchStats = async () => {
+    try {
+      loading.value = true
+      const response = await api({
+        url: '',
+        method: 'post',
+        data: {
+          query: print(findPercentageOfInterviews)
+        }
+      }) as unknown as GraphQLResponse <{ interviewsStats: StatusStats[]}>
+
+      if (response.data.data) {
+        // console.log(response.data.data.activeInterns)
+        interviewsResult.value = response.data.data.interviewsStats
+      }
+    } catch (e) {
+      console.log(e)
+    } finally {
+      loading.value = false
+    }
+  }
+  onMounted(async () => {
+    await fetchStats().catch(e => console.log(e))
+  })
+
+  return {
+    interviewsResult,
+    loading,
+    fetchStats
+  }
+}
+
+export function useFetchStatsForInterviewsPerPeriod (months: Ref<number>) {
+  const interviewsPeriodResult = ref<StatusStats[]>([])
+  const loading = ref(false)
+  console.log('MONTH SENT to interviews', months.value)
+  const fetchStats = async () => {
+    try {
+      loading.value = true
+      const response = await api({
+        url: '',
+        method: 'post',
+        data: {
+          query: print(findPercentageOfInterviewsMonthly),
+          variables: {
+            findPercentageOfInterviewsMonthly: unref(months)
+          }
+        }
+      }) as unknown as GraphQLResponse <{ interviewsPeriodStats: StatusStats[]}>
+
+      if (response.data.data) {
+        // console.log(response.data.data.activeInterns)
+        interviewsPeriodResult.value = response.data.data.interviewsPeriodStats
+      }
+    } catch (e) {
+      console.log(e)
+    } finally {
+      loading.value = false
+    }
+  }
+  onMounted(async () => {
+    await fetchStats().catch(e => console.log(e))
+  })
+  watch(months, async (val, prev) => {
+    console.log('watcher')
+    console.log('val:', val)
+    console.log('prev', prev)
+    await fetchStats()
+  })
+
+  return {
+    interviewsPeriodResult,
+    loading,
+    fetchStats
+  }
+}
