0a2dd21526551e8a974fa490886b637cbeef34d5
Stathis96 <ioannidis.stathis@yahoo.gr>
2022-02-07 18:47:19 +0200

    Create GlobalStuff.ts

---
+++ src/assets/GlobalStuff.ts
@@ -0,0 +1,87 @@
+export const positionOptions = () => {
+  return [
+    {
+      label: 'Sales',
+      value: 'sales'
+    },
+    {
+      label: 'Financial',
+      value: 'financial'
+    },
+    {
+      label: 'Marketing',
+      value: 'marketing'
+    },
+    {
+      label: 'Developer',
+      value: 'dev'
+    },
+    {
+      label: 'Network',
+      value: 'network'
+    },
+    {
+      label: 'Stores',
+      value: 'stores'
+    }
+  ]
+}
+
+export const statusOptions = () => {
+  return [
+    {
+      label: 'Pending',
+      value: 'pending'
+    },
+    {
+      label: 'Accepted',
+      value: 'accepted'
+    },
+    {
+      label: 'Rejected',
+      value: 'rejected'
+    },
+    {
+      label: 'StandBy',
+      value: 'standby'
+    }
+  ]
+}
+
+export const employmentOptions = () => {
+  return [
+    {
+      label: 'Fulltime',
+      value: 'fulltime'
+    },
+    {
+      label: 'Parttime',
+      value: 'parttime'
+    },
+    {
+      label: 'Internship',
+      value: 'internship'
+    }
+  ]
+}
+
+export const internStatusOptions = () => {
+  return [
+    {
+      label: 'Active',
+      value: 'active'
+    },
+    {
+      label: 'Employed',
+      value: 'employed'
+    },
+    {
+      label: 'Dismissed',
+      value: 'dismissed'
+    },
+    {
+      label: 'StandBy',
+      value: 'standby'
+    }
+  ]
+}
