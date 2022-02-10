c90f6049f9059151c48369155f5e2ca4c7a319a6
Stathis96 <ioannidis.stathis@yahoo.gr>
2022-02-10 18:53:58 +0200

    Update useCandidateMutations.ts
    Update candidate given an id

--- src/hooks/useCandidateMutations.ts
+++ src/hooks/useCandidateMutations.ts
@@ -55,3 +55,53 @@ export function useCandidateMutations (variables: CandidateInputData) {
     loading
   }
 }
+
+export function useCandidateUpdateMutations (variables: CandidateInputData) {
+  const result = ref<Candidate>()
+  const loading = ref(false)
+
+  const updateCandidate = async (updateCandidateId: string) => {
+    try {
+      loading.value = true
+      const response = await api({
+        url: '',
+        method: 'post',
+        data: {
+          query: print(updateCandidateMutation),
+          variables: {
+            // data: variables
+            createCandidateData: {
+              name: variables.name,
+              email: variables.email,
+              mobile: variables.mobile,
+              degree: variables.degree,
+              position: variables.position,
+              status: variables.status,
+              employmentType: variables.employmentType
+            },
+            updateCandidateId: updateCandidateId
+          }
+        }
+      }) as unknown as GraphQLResponse <{ updateCandidate: Candidate}>
+
+      if (response.data.data) {
+        result.value = response.data.data.updateCandidate
+      }
+      if (response.data.errors) {
+        console.log('show me the errors', (response.data.errors[0] as unknown as Error).message)
+        errors = (response.data.errors[0] as unknown as Error).message
+        return errors
+      }
+    } catch (e) {
+      console.log(e)
+    } finally {
+      loading.value = false
+    }
+  }
+  return {
+    updateCandidate,
+    result,
+    loading
+  }
+}
+}
