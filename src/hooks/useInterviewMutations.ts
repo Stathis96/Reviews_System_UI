8922ce4aa1fabe92ac32c44c39ef06aa885a9cfa
Stathis96 <ioannidis.stathis@yahoo.gr>
2022-02-11 14:54:18 +0200

    Update useInterviewMutations.ts
    Update too

--- src/hooks/useInterviewMutations.ts
+++ src/hooks/useInterviewMutations.ts
@@ -58,3 +58,55 @@ export function useInterviewMutations (variables: InterviewInputData) {
     loading
   }
 }
+
+export function useInterviewUpdateMutations (variables: InterviewInputData) {
+  const result = ref<Interview>()
+  const loading = ref(false)
+
+  const updateInterview = async (updateInterviewId: string) => {
+    try {
+      loading.value = true
+      const response = await api({
+        url: '',
+        method: 'post',
+        data: {
+          query: print(updateInterviewMutation),
+          variables: {
+            // data: variables
+            createInterviewData: {
+              candidateId: variables.candidateId,
+              // interviewerId: variables.interviewerId,
+              startTime: variables.startTime,
+              endTime: variables.endTime,
+              experience: variables.experience,
+              degree: variables.degree,
+              comments: variables.comments,
+              grading: variables.grading,
+              status: variables.status
+            },
+            updateInterviewId: updateInterviewId
+          }
+        }
+      }) as unknown as GraphQLResponse <{ updateInterview: Interview}>
+
+      if (response.data.data) {
+        result.value = response.data.data.updateInterview
+      }
+      if (response.data.errors) {
+        console.log('show me the errors', (response.data.errors[0] as unknown as Error).message)
+        errors = (response.data.errors[0] as unknown as Error).message
+        return errors
+      }
+    } catch (e) {
+      console.log(e)
+    } finally {
+      loading.value = false
+    }
+  }
+  return {
+    updateInterview,
+    result,
+    loading
+  }
+}
+
