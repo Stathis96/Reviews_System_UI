e431d4261a4a49565754cf6837fccae774772244
Stathis96 <ioannidis.stathis@yahoo.gr>
2022-02-14 19:37:04 +0200

    Update useReviewMutations.ts
    Update done

--- src/hooks/useReviewMutations.ts
+++ src/hooks/useReviewMutations.ts
@@ -56,3 +56,54 @@ export function useReviewMutations (variables: ReviewInputData) {
     loading
   }
 }
+
+export function useReviewUpdateMutations (variables: ReviewInputData) {
+  const result = ref<Review>()
+  const loading = ref(false)
+
+  const updateReview = async (updateReviewId: string) => {
+    try {
+      loading.value = true
+      const response = await api({
+        url: '',
+        method: 'post',
+        data: {
+          query: print(updateReviewMutation),
+          variables: {
+            // data: variables
+            createReviewData: {
+              internId: variables.internId,
+              supervisorId: variables.supervisorId,
+              createdAt: variables.createdAt,
+              initiative: variables.initiative,
+              cooperation: variables.cooperation,
+              performance: variables.performance,
+              consistency: variables.consistency,
+              total: variables.total
+            },
+            updateReviewId: updateReviewId
+          }
+        }
+      }) as unknown as GraphQLResponse <{ updateReview: Review}>
+
+      if (response.data.data) {
+        result.value = response.data.data.updateReview
+      }
+      if (response.data.errors) {
+        console.log('show me the errors', (response.data.errors[0] as unknown as Error).message)
+        errors = (response.data.errors[0] as unknown as Error).message
+        return errors
+      }
+    } catch (e) {
+      console.log(e)
+    } finally {
+      loading.value = false
+    }
+  }
+  return {
+    updateReview,
+    result,
+    loading
+  }
+}
+
